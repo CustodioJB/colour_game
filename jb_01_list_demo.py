50c21654f60871d909fe9dff8c4d6d02829ad6b6
CustodioJB <100053960+CustodioJB@users.noreply.github.com>
2024-04-02 21:33:04 +1300

    Created Rounds GUI

---
+++ jb_01_list_demo.py
@@ -0,0 +1,12 @@
+some_colors = [
+    ['alice blue',    '#F0F8FF','19', 'black'],
+    ['antique white', '#FAEBD7', '20', 'black'],
+    ['AntiqueWhite1', '#FFEFDB','20', 'black'], 
+    ['AntiqueWhite2', '#EEDFCC','19', 'black'],
+    ['AntiqueWhite3', '#CDC0B0','16', 'black'], 
+    ['AntiqueWhite4', '#8B8378','11', 'white']
+]
+
+
+for item in some_colors:
+    print(item[0])
